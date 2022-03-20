17d878719f9455b336e28ba7777ac990f60e1433
tgsmikema <32582220+tgsmikema@users.noreply.github.com>
2022-03-20 13:08:09 +0800

    implemented split_string_into_tokens() function

--- ash.c
+++ ash.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 /*  Author: Siqi(Mike) Ma
     ID:     2623324
@@ -16,7 +17,7 @@
 char* read_command_line_from_input(void){
 
     char *command = NULL;
-    size_t buffsize = 0;
+    ssize_t buffsize = 0;
 
     /* some of below code referenced from https://brennan.io/2015/01/16/write-a-shell-in-c/
     TOTAL REFERENCE: (6) LINES */
@@ -34,7 +35,51 @@ char* read_command_line_from_input(void){
         }
     }
 
+    // get rid of new line character parsed into the string.
+    if ((command)[strlen(command) - 1] == '\n') 
+    {
+      ((command)[strlen(command) - 1] = '\0');
+    }
+
     return command;
+}
+
+char** split_string_into_tokens(char* input_string){
+
+    //printf("%s",input_string);
+    const char delim[2] = " ";
+    char* token;
+    
+    // calculate the how many space seperated words in the input string.
+    int word_count = 1;
+    for(int i=0;i<strlen(input_string);i++){
+        if (input_string[i] == ' '){
+            word_count++;
+        }
+    }
+
+    // allocate enough memory for string array.
+    // (+1) make space for NULL at the end of token. 
+    char **tokens = (char **) malloc((word_count+1) * sizeof(char*));
+
+    // error checking for tokens.
+    if (tokens == NULL){
+        perror("memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(input_string, delim);
+
+    int i = 0;
+    while(token != NULL) {
+        tokens[i] = token;
+        i++;  
+        token = strtok(NULL, delim);
+   }
+
+    tokens[i] = NULL;
+    return tokens;
+
 }
 
 int main(int argc, char* argv[]){
@@ -49,8 +94,12 @@ int main(int argc, char* argv[]){
     // otherwise if from file, then call read_command_line_from_file() function.
     if (isatty(STDIN_FILENO) == 1){
         char *line = read_command_line_from_input();
-        // developer test line:
-        printf("%s",line);
+    
+        sleep(1);
+        char **convert = split_string_into_tokens(line);
+
+        // developer test: while(*convert != NULL){printf("%s\n",*convert);convert++;}
+        // execvp(convert[0],convert);
     } else {
 
     }
